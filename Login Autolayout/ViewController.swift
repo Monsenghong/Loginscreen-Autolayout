c89b4cecb3233868148003432e51c32f3e9efc2a
Senghong <senghong601@gmail.com>
2022-02-11 13:08:37 +0700

    Login screen auto layout

--- Login Autolayout/ViewController.swift
+++ Login Autolayout/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var logoImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        phoneTextField.setLeftPaddingPoints(30)
+        phoneTextField.setBottomBorder()
+        logoImageView.makeRounded()
+        passwordTextField.setBottomBorder()
+        passwordTextField.setLeftPaddingPoints(30)
+        
     }
 
 
+}
+
+
+extension UITextField {
+    func setLeftPaddingPoints(_ amount:CGFloat){
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.leftView = paddingView
+        self.leftViewMode = .always
+    }
+    func setRightPaddingPoints(_ amount:CGFloat) {
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+        self.rightView = paddingView
+        self.rightViewMode = .always
+    }
+}
+
+extension UITextField {
+  func setBottomBorder() {
+    self.borderStyle = .none
+    self.layer.backgroundColor = UIColor.white.cgColor
+    
+    self.layer.masksToBounds = false
+    self.layer.shadowColor = UIColor.gray.cgColor
+    self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+    self.layer.shadowOpacity = 1.0
+    self.layer.shadowRadius = 0.0
+  }
+}
+
+
+
+
+extension UIImageView {
+
+    func makeRounded() {
+        
+        self.layer.borderWidth = 1
+        self.layer.masksToBounds = false
+        self.layer.borderColor = UIColor.purple.cgColor
+        self.layer.cornerRadius = self.frame.height / 2
+        self.clipsToBounds = true
+    }
 }
 
